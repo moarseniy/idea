7e683c959e9f4f189464017ab2bcdb9a9ae0ca90
moarseniy <moarseniy@yandex.ru>
2025-10-01 09:34:08 +0300

    fix: fixed json agent names

--- gradio_service/scripts/json_analytic_pipeline.py
+++ gradio_service/scripts/json_analytic_pipeline.py
@@ -15,28 +15,28 @@ def run_compute_json_profile(path):
     return profile
 
 
-def run_final_profile(profile, json_answer_patch):
+def run_final_json_profile(profile, json_answer_patch):
     
     # patch - это  ответ от LLM - Просим нам LLM правильно назвать сущности и поля бизнесово.
     # result = validate_rename_patch(profile, json_answer_patch)  # критик - он уже сам скажет какие ошибки в ответе и если все норм, то SUCCESS
     # print("(validate_rename_patch):", result)
     
-    final_prof = build_final_profile(profile, json_answer_patch)
+    final_profile = build_final_profile(profile, json_answer_patch)
 
     return final_profile
 
 
-def get_clickhouse_ddl(final_prof):
+def json_get_clickhouse_ddl(final_prof):
     ddl_ch = emit_ddl_ch(final_prof, types_yaml_path="config/types.yaml", database="mydb")  # database опционально
     return ddl_ch
 
 
-def get_postgres_ddl(final_prof):
+def json_get_postgres_ddl(final_prof):
     ddl_pg = emit_ddl_pg(final_prof, types_yaml_path="config/types.yaml")
     return ddl_pg
 
 
-def get_dbml(final_prof):
+def json_get_dbml(final_prof):
     dbml_text = emit_dbml(final_prof, project_database_type="Generic")
     print(dbml_text[:500], "...")
 
